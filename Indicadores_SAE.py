8aecf66cccf5d0803e1dfb64674626dffb659016
hugobovareto <hugobovareto@gmail.com>
2026-08-07 09:50:38 -0300

    Primeiro commit

---
+++ Indicadores_SAE.py
@@ -0,0 +1,99 @@
+'''
+
+
+'''
+# Importação das bibliotecas
+import pandas as pd
+import glob
+import os
+from tqdm import tqdm  # Para barra de progresso
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+import openpyxl
+import re
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
